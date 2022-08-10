0ca00547b93d8d26551b54ab9a4f67e07f425690
David Gnamuš <38346533+dgnamus@users.noreply.github.com>
2022-08-10 21:24:39 +0200

    Create fizzBuzz.js

---
+++ fizzBuzz.js
@@ -0,0 +1,12 @@
+const limit = 25;
+for (let count = 1; count <= 100; count++) {
+  if (count % 3 === 0 && count % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (count % 3 === 0 && !count % 5 === 0) {
+    console.log("Fizz");
+  } else if (!count % 3 === 0 && count % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(count);
+  }
+}
